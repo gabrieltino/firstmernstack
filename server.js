f0e53776d6e52d865beb2292ea8f56bddb949a4c
gabrieltino <miracle8093@gmail.com>
2019-06-25 18:04:30 +0100

    Mern stack check

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/items", items);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client', 'build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
