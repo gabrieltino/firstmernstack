db988ad4423f22e933468ae548083c38e719a5e0
gabrieltino <miracle8093@gmail.com>
2019-06-25 20:13:14 +0100

    removed redux devtools

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ if (process.env.NODE_ENV === 'production') {
   //set static folder
   // app.use('/static', express.static(path.join(__dirname, 'client/build')));
   // app.use(express.static('client/build'));
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
