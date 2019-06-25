b0ffb06d5d693f4663a05d6fb2e8ac3f7b2cf014
gabrieltino <miracle8093@gmail.com>
2019-06-25 13:54:47 +0100

    Integrated Redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import {Container} from 'reactstrap'
+
+import { Provider } from 'react-redux'
+import store from './store'
+import ItemModal from './components/itemModal';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+        <ItemModal />
       <ShoppingList />
+       </Container>
     </div>
+    </Provider>
   );
 }
 
