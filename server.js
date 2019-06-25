f4f89edc5d637ff95fb0da143a13d10ec7ef7812
gabrieltino <miracle8093@gmail.com>
2019-06-25 19:46:35 +0100

    make it better2

--- server.js
+++ server.js
@@ -25,11 +25,10 @@ app.use("/api/items", items);
 //serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  // app.use('/static', express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname+'client/build/index.html'));
   });
 }
 
