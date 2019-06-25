89556896ec5f8686fe8b927b2dc2562a36ece7ec
gabrieltino <miracle8093@gmail.com>
2019-06-25 20:05:58 +0100

    make it better

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.use("/api/items", items);
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   // app.use('/static', express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
